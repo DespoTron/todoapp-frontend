9770c876a2bceff028bcf5f4ed09da7e3544b9e3
Thai Do <thai.hl.do@gmail.com>
2021-01-27 12:42:44 -0800

    Added png files and updating README.md

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -12,7 +12,7 @@ const InputTodo = () => {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(textBody),
       })
-      console.log(response)
+      window.location = '/'
     } catch (err) {
       console.error(err.message)
     }
